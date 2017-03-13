96f19eb154432ca6e3e10ee54e474395f5aa6b54
anontyro <alexwilkinson@gmail.com>
2017-03-13 23:09:07 +0800

    REST controller created and main webapp controller create
    the basic outline is now in place, next I need to add the ability to
    serve static files such as css and javascript

--- src/main/java/co/alexwilkinson/DaintreeApplication.java
+++ src/main/java/co/alexwilkinson/DaintreeApplication.java
@@ -1,34 +1,33 @@
 package co.alexwilkinson;
 
-import java.util.Arrays;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.Bean;
+
 
 @SpringBootApplication
-public class DaintreeApplication {
-
-	public static void main(String[] args) {
+public class DaintreeApplication{
+	
+	public static void main(String[] args){
 		SpringApplication.run(DaintreeApplication.class, args);
 	}
 	
-	@Bean
-	public CommandLineRunner commandLineRunner(ApplicationContext ctx){
-		
-		return args ->{
-			
-			System.out.println("Beans in Java Spring");
-			String[] beanNames = ctx.getBeanDefinitionNames();
-			Arrays.sort(beanNames);
-			for(String beanName : beanNames){
-				System.out.println(beanName);
-			}
-		};
-		
-	}
+
+	
+//	@Bean
+//	public CommandLineRunner commandLineRunner(ApplicationContext ctx){
+//		
+//		return args ->{
+//			
+//			System.out.println("Beans in Java Spring");
+//			String[] beanNames = ctx.getBeanDefinitionNames();
+//			Arrays.sort(beanNames);
+//			for(String beanName : beanNames){
+//				System.out.println(beanName);
+//			}
+//		};
+//		
+//	}
 	
 	
 }
