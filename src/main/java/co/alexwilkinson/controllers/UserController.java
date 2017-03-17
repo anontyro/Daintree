695259fb5858fb1faff0305194ae903b3eca3db1
anontyro <alexwilkinson@gmail.com>
2017-03-17 16:40:00 +0800

    Basic Create function added
    I have added a way to generate a new user via the form, will next need
    to list the users in the database

--- src/main/java/co/alexwilkinson/controllers/UserController.java
+++ src/main/java/co/alexwilkinson/controllers/UserController.java
@@ -1,5 +1,6 @@
 package co.alexwilkinson.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,9 +8,13 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import co.alexwilkinson.models.User;
+import co.alexwilkinson.models.UserDao;
 
 @Controller
 public class UserController {
+	
+	@Autowired
+	private UserDao userDao;
 
 	@GetMapping("/userform")
 	public String userForm(Model model){
@@ -19,6 +24,14 @@ public class UserController {
 	
 	@PostMapping("/userform")
 	public String userSubmit(@ModelAttribute User user){
+		String userId = "";
+		try{
+			userDao.save(user);
+			userId = String.valueOf(user.getIdmainuser());
+		}catch(Exception ex){
+			ex.printStackTrace();
+			
+		}
 		return "userformresult";
 	}
 }
